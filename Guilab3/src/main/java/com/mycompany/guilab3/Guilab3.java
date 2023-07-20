e362cd4c9d9bae318138dea5b2aa68794789271f
ayesh20 <135942697+ayesh20@users.noreply.github.com>
2023-07-20 21:13:15 +0530

    lab 11 2023/07/20 q3

---
+++ Guilab3/src/main/java/com/mycompany/guilab3/Guilab3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guilab3;
+
+/**
+ *
+ * @author shali
+ */
+public class Guilab3 {
+
+    public static void main(String[] args) {
+        Newfram3 n3= new Newfram3();
+        n3.show();
+    }
+}
