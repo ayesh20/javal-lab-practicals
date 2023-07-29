71ab80a29d47f38a9ddb6e543bf3ab432ba876a5
ayesh20 <135942697+ayesh20@users.noreply.github.com>
2023-07-29 13:36:10 +0530

    practical 04 q2

---
+++ TestInheritance/src/main/java/com/mycompany/testinheritance/SuperB.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testinheritance;
+
+/**
+ *
+ * @author shali
+ */
+class SuperB {
+   int x;
+    public void setIt (int n) { 
+        x=n;
+    }
+    public void increase () { 
+        x=x+1;
+    }
+    public void triple () {
+        x=x*3;
+    }
+    public int returnIt () {
+        return x;} 
+}
---
+++ TestInheritance/src/main/java/com/mycompany/testinheritance/TestInheritance.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testinheritance;
+
+/**
+ *
+ * @author shali
+ */
+public class TestInheritance {
+
+    public static void main(String[] args) {
+        SuperB b = new SuperB();
+        b.setIt(2);
+        b.increase();
+        b.triple();
+        System.out.println( b.returnIt() ); 
+        SubC c = new SubC();
+        c.setIt(2);
+        c.increase();
+        c.triple();
+        System.out.println( c.returnIt() ); }
+
+    
+}
